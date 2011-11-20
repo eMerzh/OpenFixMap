d261357974c2d55255eff24bbd1a4c721378a20c
Brice Maron <brice@bmaron.net>
2011-11-20 18:05:37 +0100

    Initial import

---
+++ src/net/bmaron/openfixmap/OpenFixMapActivity.java
@@ -0,0 +1,144 @@
+package net.bmaron.openfixmap;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.PopupWindow;
+
+import org.osmdroid.util.BoundingBoxE6;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController.AnimationType;
+import org.osmdroid.views.MapView;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.SimpleLocationOverlay;
+import org.osmdroid.views.util.constants.MapViewConstants;
+
+		
+public class OpenFixMapActivity extends Activity {
+
+
+    protected MapView mapView;
+	
+    private MapController mapController;
+    private SimpleLocationOverlay mMyLocationOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;  
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapview);
+
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+
+        /* Set position near etterbeek */
+        mapController = this.mapView.getController();
+        mapController.setZoom(16);
+        GeoPoint p = new GeoPoint(50.838599, 4.406551);
+        mapController.setCenter(p);
+
+
+        this.mMyLocationOverlay = new SimpleLocationOverlay(this);                          
+        this.mapView.getOverlays().add(mMyLocationOverlay);
+       
+        this.mScaleBarOverlay = new ScaleBarOverlay(this);                          
+        this.mapView.getOverlays().add(mScaleBarOverlay);
+        
+
+
+        
+    	class myItemGestureListener<T extends OverlayItem> implements OnItemGestureListener<T> {
+       	 
+		    @Override
+		    public boolean onItemSingleTapUp(int index, T item) {
+		        org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(OpenFixMapActivity.class);
+		        
+		        logger.info("Hello 1 "+item.mDescription);
+		        
+		        new AlertDialog.Builder(OpenFixMapActivity.this)
+		        .setTitle(item.getTitle())
+		        .setMessage(item.mDescription)
+		        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+		            public void onClick(DialogInterface dialog, int which) { 
+		                // continue with delete
+		            }
+		         })
+		        /*.setNegativeButton("No", new DialogInterface.OnClickListener() {
+		            public void onClick(DialogInterface dialog, int which) { 
+		                // do nothing
+		            }
+		         })*/
+		         .show();
+
+		        return false;
+		    }
+		 
+	    @Override
+		    public boolean onItemLongPress(int index, T item) {
+	        	org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(OpenFixMapActivity.class);
+	    		logger.info("Hello 2");
+		        return false;
+		    }
+    	}
+
+        OnItemGestureListener<OverlayItem> pOnItemGestureListener = new myItemGestureListener<OverlayItem>();
+        org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(OpenFixMapActivity.class);
+
+
+
+        
+        String next[] = {};
+        List<String[]> list = new ArrayList<String[]>();
+
+        try {
+            CSVReader reader = new CSVReader(new InputStreamReader(getAssets().open("points.csv")),'\t', '\0', 0);
+            for(;;) {
+                next = reader.readNext();
+                if(next != null) {
+                    list.add(next);
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        List<OverlayItem> pList = new ArrayList<OverlayItem>();
+        for(int i=1; i<= 10; i++) {
+        	logger.info("array is"+Arrays.toString(list.get(i)));
+        	if(list.get(i).length < 12) continue;
+        	logger.info("Hello "+list.get(i)[0]+" World "+list.get(i)[1]);
+        	GeoPoint point = new GeoPoint(Double.parseDouble(list.get(i)[0]), Double.parseDouble(list.get(i)[1]));
+            OverlayItem myItem = new OverlayItem(list.get(i)[2],list.get(i)[10],point);
+            pList.add(myItem);
+
+        }
+
+        ItemizedIconOverlay<OverlayItem> test = new ItemizedIconOverlay<OverlayItem>(this, pList, pOnItemGestureListener);
+        this.mapView.getOverlays().add(test);
+        
+        logger.info("Hello World"+ this.mapView.getOverlays().size());
+
+    }
+}
