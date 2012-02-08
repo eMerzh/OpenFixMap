08906fef8f316b546a8bff68e4787083659a39e4
Brice Maron <brice@bmaron.net>
2012-02-08 20:42:13 +0100

    Clean up test for closed item

--- src/net/bmaron/openfixmap/OpenFixMapActivity.java
+++ src/net/bmaron/openfixmap/OpenFixMapActivity.java
@@ -218,14 +218,12 @@ public class OpenFixMapActivity extends Activity {
         for(int i=0; i < itemList.size(); i++) {
         	ErrorItem item = itemList.get(i);
         	OverlayErrorItem oItem = new OverlayErrorItem(item);
-			if(item.getErrorStatus() == 2) { 
-				// ToDo: Better check against constant from ErrorItem, e.g. ErrorStatus == ErrorItem.ST_CLOSE 
+			if(item.getErrorStatus() == ErrorItem.ST_CLOSE) { 
 				oItem.setMarker( res.getDrawable(R.drawable.resolved));
 			} else {
 			   	oItem.setMarker( res.getDrawable(R.drawable.caution));
 			}
         	overlayList.add(oItem);
-            //num_item = itemList.size();
         }
         mapView.getOverlays().remove(pointOverlay);
         createPointOverlay(overlayList);//Change pointOverlay
